7b3a36c14b0c15594d5a657ca8d07204a30d77de
J3n50m4t <J3n50m4t@users.noreply.github.com>
2023-04-26 08:39:20 +0200

    Adding HA Addon files

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/with-contenv bashio
+
+bashio::log.info "Starting npm server..."
+
+export HA_BASE_URL="$(bashio::config 'HA_BASE_URL')"
+export HA_SCREENSHOT_URL=$(bashio::config 'HA_SCREENSHOT_URL')
+export HA_ACCESS_TOKEN="$(bashio::config 'HA_ACCESS_TOKEN')"
+export LANGUAGE=$(bashio::config 'LANGUAGE')
+export CRON_JOB=$(bashio::config 'CRON_JOB')
+export RENDERING_TIMEOUT=$(bashio::config 'RENDERING_TIMEOUT')
+export RENDERING_DELAY=$(bashio::config 'RENDERING_DELAY')
+export RENDERING_SCREEN_HEIGHT=$(bashio::config 'RENDERING_SCREEN_HEIGHT')
+export RENDERING_SCREEN_WIDTH=$(bashio::config 'RENDERING_SCREEN_WIDTH')
+export ROTATION=$(bashio::config 'ROTATION')
+export SCALING=$(bashio::config 'SCALING')
+export GRAYSCALE_DEPTH=$(bashio::config 'GRAYSCALE_DEPTH')
+export COLOR_MODE=$(bashio::config 'COLOR_MODE')
+
+bashio::log.info "Using base_url: ${HA_BASE_URL}"
+
+cd /app
+exec /usr/bin/npm start
