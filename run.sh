2484a114c392bf61f019bf90eed6d6833b71fd05
Sebastian Schubotz <github@sibbl.net>
2024-02-22 15:45:16 +0100

    Add missing config options to run.sh

--- run.sh
+++ run.sh
@@ -11,10 +11,12 @@ export RENDERING_TIMEOUT=$(bashio::config 'RENDERING_TIMEOUT')
 export RENDERING_DELAY=$(bashio::config 'RENDERING_DELAY')
 export RENDERING_SCREEN_HEIGHT=$(bashio::config 'RENDERING_SCREEN_HEIGHT')
 export RENDERING_SCREEN_WIDTH=$(bashio::config 'RENDERING_SCREEN_WIDTH')
+export BROWSER_LAUNCH_TIMEOUT=$(bashio::config 'BROWSER_LAUNCH_TIMEOUT')
 export ROTATION=$(bashio::config 'ROTATION')
 export SCALING=$(bashio::config 'SCALING')
 export GRAYSCALE_DEPTH=$(bashio::config 'GRAYSCALE_DEPTH')
 export COLOR_MODE=$(bashio::config 'COLOR_MODE')
+export PREFERS_COLOR_SCHEME=$(bashio::config 'PREFERS_COLOR_SCHEME')
 export HA_BATTERY_WEBHOOK=$(bashio::config 'HA_BATTERY_WEBHOOK')
 
 bashio::log.info "Loading additional environment variables..."
